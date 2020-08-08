23c965c11bbc78a0f2630f395045a3b19fdf7bb2
Greg Kluska <kluskagreg@gmail.com>
2020-08-08 21:39:36 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '69f437ec27e1ec28983244d8cd635642'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '96e1cf2a-e8a3-46ff-be45-3fd91058e6fe'
+};
